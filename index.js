6f3e9c9e976a355d2028f26b341efd952fcd9919
Kenneth Rasmussen <kentora@kentora.dk>
2020-12-03 21:50:44 +0100

    tmi - twitch connected

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
 const OBSWebSocket = require('obs-websocket-js');
 const readline = require('readline');
-const { stdin } = require('process');
 
 const obs = new OBSWebSocket();
-obs.connect();
 
-readline.emitKeypressEvents(process.stdin);
+const tmi = require('tmi.js');
+
+const chat = new tmi.client({
+    connection: {
+        secure: true,
+        reconnect: true
+    },
+    channels: ['1kentora']
+})
+
+chat.on('message', (channel, user, message, self) => {
+    if(self) return; // ignore echo, but should not happen
+
+    if(isModOrHigher(user, channel) && message == "!flush"){
+        fixTheStuff();
+    }
+});
+
+readline.createInterface(process.stdin, process.stdout);
 
 process.stdin.on('keypress', (str, key) => {
     if (key.ctrl && key.name == 'c') {
         process.exit();
     }
+
+    console.log();
+
+    if(key.name == 't'){
+        chat.connect().then(() => {
+            console.log("Twitch chat connected");
+        }).catch(err => console.error(err));
+
+        return;
+    }
+
+    if(key.name == 'f'){
+        fixTheStuff();
+        return;
+    }
+    
+    if(key.name == 'o'){
+        obs.connect().then(() => {
+            console.log("OBS connected");
+        }).catch("obs not connected");
+    }
+
+    console.log("t for twitch connect, o for obs connect, f for fix, ctrl+c for exit");
+});
+
+function fixTheStuff(){
     obs.send('SetSceneItemProperties', { item: { name: 'RTMP' }, visible: false }).then(() => {
         setTimeout(() => {
             obs.send('SetSceneItemProperties', { item: { name: 'RTMP' }, visible: true }).then(() => {
@@ -18,4 +60,10 @@ process.stdin.on('keypress', (str, key) => {
             })
         }, 200);
     });
-});
+}
+
+function isModOrHigher(user, channel){
+    let isMod = user.mod || user['user-type'] === 'mod';
+    let isBroad = channel.slice(1) === user.username
+    return isMod || isBroad;
+}
