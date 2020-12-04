eddeb31de24d4363c03b1f4761eeafc7ed8facb1
Kenneth Rasmussen <kentora@kentora.dk>
2020-12-04 16:37:26 +0100

    Autoconnect + env variables

--- index.js
+++ index.js
@@ -4,26 +4,62 @@ const readline = require('readline');
 const obs = new OBSWebSocket();
 
 const tmi = require('tmi.js');
+const { throws } = require('assert');
 
 const chat = new tmi.client({
     connection: {
         secure: true,
         reconnect: true
     },
-    channels: ['1kentora']
+    channels: [process.env['CHANNEL']]
 })
 
 let lastFlush = 0;
 
+let obsConnected = false;
+let twitchConnected = false;
+
 chat.on('message', (channel, user, message, self) => {
     if(self) return; // ignore echo, but should not happen
 
-    if(isModOrHigher(user, channel) && message == "!flush"){
+    if(isModOrHigher(user, channel) && message == process.env['COMMAND']){
         if(isAfterTimeout()){
             fixTheStuff();
         }
     }
 });
+
+obs.on('ConnectionClosed', () => {
+    obsConnected = false;
+    console.log("OBS DISCONNECTED");
+});
+
+obs.on('ConnectionOpened', () => {
+    obsConnected = true;
+    console.log("OBS CONNECTED");
+});
+
+chat.on('disconnected', () => {
+    console.log("TWITCH DISCONNECTED");
+    twitchConnected = false;
+});
+
+chat.on('connected', () => {
+    console.log("TWITCH CONNECTED");
+    twitchConnected = true;
+});
+
+setInterval(() => {
+    if(!obsConnected){
+        obs.connect().catch(err => {
+            console.debug("Could not connect to OBS", err);
+        });
+    }
+
+    if(!twitchConnected){
+        chat.connect().catch(err => console.debug("Could not connect to twitch", err));
+    }
+}, 1000);
 
 readline.createInterface(process.stdin, process.stdout);
 
@@ -34,36 +70,27 @@ process.stdin.on('keypress', (str, key) => {
 
     console.log();
 
-    if(key.name == 't'){
-        chat.connect().then(() => {
-            console.log("Twitch chat connected");
-        }).catch(err => console.error(err));
-
-        return;
-    }
-
     if(key.name == 'f'){
         fixTheStuff();
         return;
     }
-    
-    if(key.name == 'o'){
-        obs.connect().then(() => {
-            console.log("OBS connected");
-        }).catch("obs not connected");
+
+    if(key.name == 's'){
+        console.log("Status:\tTwitch " + (twitchConnected ? "connected" : "disconnected") + "\tOBS " + (obsConnected ? "connected" : "disconnected"))
+        return;
     }
 
-    console.log("t for twitch connect, o for obs connect, f for fix, ctrl+c for exit");
+    console.log("s for status, f for fix, ctrl+c for exit");
 });
 
 function fixTheStuff(){
-    obs.send('SetSceneItemProperties', { item: { name: 'RTMP' }, visible: false }).then(() => {
+    obs.send('SetSceneItemProperties', { item: { name: process.env['ITEM_NAME'] }, visible: false }).then(() => {
         setTimeout(() => {
-            obs.send('SetSceneItemProperties', { item: { name: 'RTMP' }, visible: true }).then(() => {
+            obs.send('SetSceneItemProperties', { item: { name: process.env['ITEM_NAME'] }, visible: true }).then(() => {
                 console.log("I fixed all the shiiiitz");
-            })
+            }).catch(err => console.error("Could not set visibility true again", err));
         }, 200);
-    });
+    }).catch(err => console.error("Could not flush", err));
     lastFlush = Date.now();
 }
 
@@ -74,5 +101,5 @@ function isModOrHigher(user, channel){
 }
 
 function isAfterTimeout(){
-    return (Date.now() - lastFlush) > 30000;
+    return (Date.now() - lastFlush) > parseInt(process.env['TIMEOUT']);
 }
--- start.bat
+++ start.bat
@@ -1,3 +1,12 @@
 @echo off
 
+:: The name of the item that should be hidden
+set ITEM_NAME=RTMP
+:: The Twitch channel to look for
+set CHANNEL=1kentora
+:: Timeout before allowed invocations
+set TIMEOUT=30000
+:: Command to listen for
+set COMMAND=!flush
+
 node index.js
